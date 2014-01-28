1f1dd2f35e0adf264badc082ac2be05b0b0d7622
Joe Sullivan <josulliv101@gmail.com>
2014-01-28 00:32:41 -0500

    Panels

--- app/scripts/views/searchpanels/Location.js
+++ app/scripts/views/searchpanels/Location.js
@@ -20,6 +20,18 @@ define([
 
             Base.prototype.initialize.call(this);
 
+            this.nav = [
+
+                            { navid: 'details', order: 1, navlabel: 'Details' },
+
+                            { navid: 'photo', order: 2, navlabel: 'Photo' },
+
+                            { navid: 'offices', order: 3, navlabel: 'Departments &amp; Offices' },
+
+                            { navid: 'directions', order: 4, navlabel: 'Directions' }
+
+                       ];
+
             this.listenTo(EventDispatcher, 'change:detailsview', function(panelid) {
 
                 model.set('detailsview', panelid, { silent: true });
@@ -38,11 +50,48 @@ define([
 
                 location = map.details, //_.first(map.selectedLocations), //map.locationDetails,
 
-                json = Datastore.JSON.location(location);
+                json = Datastore.JSON.location(location),
+
+                viewid = this.model.get('detailsview') || _.find(this.nav, function(item) { return item.order === 1; }).navid;
+
+            location.detailsnav || (location.detailsnav = this.getNavModel(location, viewid));
 
             json.detailsview = this.model.get('detailsview') || 'details';
 
+            json.nav = _.chain(location.detailsnav)
+
+                        .each(function(item) { item.active = (item.navid === viewid ? 'active' : null); })
+
+                        .value();
+                 debugger;       
             return { data: json };
+
+        },
+
+        getNavModel: function (loc, viewid) {
+
+            var nav = _.clone(this.nav),
+
+                occupants = loc.occupants,
+
+                urlphoto = loc.urlphoto,
+
+                officesItem = this.getNavItemById(nav, 'offices');
+
+            // Hide the offices item if none
+            if (!occupants || _.size(occupants) === 0)  nav = _.without(nav, officesItem);
+
+            if (!urlphoto || urlphoto === '')  nav = _.without(nav, this.getNavItemById(nav, 'photo'));
+
+            if (_.size(occupants) > 0) officesItem.navlabel = officesItem.navlabel.replace(/\([0-9]+\)/, "") + " (" + _.size(occupants)+ ")";
+
+            return nav;
+
+        },
+
+        getNavItemById: function(nav, id) {
+
+            return _.find(nav, function(item) { return item.navid === id; });
 
         },
 
